d75d0104d580729bfc77c10b9cac834a7df7ef49
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-04 05:12:10 -0700

    added getchar.c file

---
+++ getchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ab;
+    printf("Enter a Character: ");
+    ab = getchar();
+    printf("\nThe ASCII value of %c is %d \n \n " , ab ,ab );
+}
