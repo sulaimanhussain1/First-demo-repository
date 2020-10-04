08d10b159ec5d2df2a3e06c531cc632c834cb31d
Sulaiman Hussain <hussainsulaiman460@gmail.com>
2020-10-04 04:02:41 -0700

    added add.c file.

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    // add two numbers...
+    int a,b;
+    printf("Enter value of A: \n");
+    scanf("%d" , &a);
+
+     printf("Enter value of B: \n");
+    scanf("%d" , &b);
+
+    printf("The sum is %d\n " , a+b);
+
+    return 0;
+}
